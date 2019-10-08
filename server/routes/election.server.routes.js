16724c1db9d840715e633ccdb531e65e03a0d106
hw5711 <huanwu5711@gmail.com>
2019-10-08 11:36:31 -0500

    update

--- server/routes/election.server.routes.js
+++ server/routes/election.server.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var mongoose = require('mongoose');
 
-const Election = require("../models/election");
+//const Election = require("../models/election");
 const checkAuth = require("../middleware/check-auth");
 
 const John = require("../models/polljohn");
@@ -13,12 +13,10 @@ const app = express.Router();
 //use for updating the account info
 app.post("/vote", (req, res, next) => {
 
-    Election.find({creator: req.body.creator}, )
-    
-    Election.create(req.body, function (err, post) {
-        if (err) return next(err);
-        console.log(post);
-    });
+    John.fin
+    if(req.body.name === "john"){
+        John.create({})
+    }
 });
 
 module.exports = app;
